760e6ee5fecb9bf81a807a461b65e899799b3b58
gülzade odabaş <gulzadeodabas27@gmail.com>
2021-01-23 18:13:36 +0300

    Add project files.

---
+++ DictonaryDemo/MyDictonary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryDemo
+{
+    class MyDictonary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictonary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void WriteDictonary()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i]);
+                Console.WriteLine(values[i]);
+            }
+        }
+    }
+}
---
+++ DictonaryDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictonaryDemo
+{
+    class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> myDictonary = new MyDictonary<int, string>();
+
+            myDictonary.Add(1, "Psikoloji Kitapları");
+            myDictonary.Add(2, "Edebiyat Kitapları");
+            myDictonary.Add(3, "Eğitim Kitapları");
+            myDictonary.Add(4, "Anı Kitapları");
+
+            myDictonary.WriteDictonary();
+            
+
+        }
+    }
+
+    
+}
